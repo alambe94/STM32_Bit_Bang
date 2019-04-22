0414623a3a8e8a25e87cc774ec3531804ba4456d
alsaadlambe <lambealsaad@.com>
2019-04-22 14:34:26 +0530

    update

--- STM32_Bit_Bang_UART/Inc/soft_uart.h
+++ STM32_Bit_Bang_UART/Inc/soft_uart.h
@@ -32,6 +32,7 @@ typedef struct Soft_Uart_t
 	char    RX_Byte;
 
 	uint8_t TX_Active_Flag;
+	uint8_t RX_Active_Flag;
 
 	uint8_t TC_Flag; //transmission complete flag
 	uint8_t RC_Flag;
@@ -41,6 +42,11 @@ typedef struct Soft_Uart_t
     void Soft_Uart_Init(Soft_Uart_t* handle);
     void Soft_Uart_Send_Char(Soft_Uart_t* uart_handle, char data);
     void Soft_Uart_Send_String(Soft_Uart_t* uart_handle, char* str);
+    void Soft_Uart_Start();
+    void Soft_Uart_Stop();
+    uint8_t Soft_Uart_Get_TC_Flag(Soft_Uart_t* uart_handle);
+    void Soft_Uart_Clear_TC_Flag(Soft_Uart_t* uart_handle);
+
 
 
 
--- STM32_Bit_Bang_UART/Src/soft_uart.c
+++ STM32_Bit_Bang_UART/Src/soft_uart.c
@@ -18,6 +18,21 @@ void Soft_Uart_Init(Soft_Uart_t* uart_handle)
 
      if(Soft_Uart_Count< MAX_SOFT_UART)
 	 {
+
+	 uart_handle->RC_Flag = 0;
+	 uart_handle->RX_Active_Flag = 0;
+	 uart_handle->RX_Bit_Count = 0;
+	 uart_handle->RX_Byte = 0x00;
+	 uart_handle->RX_Ring_Buffer.Read_Index = 0;
+	 uart_handle->RX_Ring_Buffer.Write_Index = 0;
+
+	 uart_handle->TC_Flag = 0;
+	 uart_handle->TX_Active_Flag = 0;
+	 uart_handle->TX_Bit_Count = 0;
+	 uart_handle->TX_Byte = 0;
+	 uart_handle->TX_Ring_Buffer.Read_Index = 0;
+	 uart_handle->TX_Ring_Buffer.Write_Index = 0;
+
 	 Soft_Uart_List[Soft_Uart_Count] = uart_handle;
 	 Soft_Uart_Count++;
 	 }
@@ -28,18 +43,20 @@ void _104us_ISR()
     {
 
     Soft_Uart_t* uart_handle = NULL;
-    Uart_Ring_Buffer* ring_buffer_handle = NULL;
+    Uart_Ring_Buffer* tx_buffer_handle = NULL;
+    Uart_Ring_Buffer* rx_buffer_handle = NULL;
     uint8_t uart_active_count = 0;
 
     for (uint8_t i = 0; i < Soft_Uart_Count; i++)
 	{
 
 	uart_handle = Soft_Uart_List[i];
-	ring_buffer_handle = &uart_handle->TX_Ring_Buffer;
+	tx_buffer_handle = &uart_handle->TX_Ring_Buffer;
+	rx_buffer_handle = &uart_handle->RX_Ring_Buffer;
 
 	if (!uart_handle->TX_Active_Flag)
 	    {
-	    if (Ring_Buffer_Get_Count(ring_buffer_handle))
+	    if (Ring_Buffer_Get_Count(tx_buffer_handle))
 		{
 		uart_handle->TX_Active_Flag = 1;
 		}
@@ -56,7 +73,7 @@ void _104us_ISR()
 
 	    if (!(uart_handle->TX_Bit_Count)) //start bit
 		{
-		Ring_Buffer_Get_Char(ring_buffer_handle, &uart_handle->TX_Byte);
+		Ring_Buffer_Get_Char(tx_buffer_handle, &uart_handle->TX_Byte);
 		HAL_GPIO_WritePin(uart_handle->GPIO_TX_Port,
 			uart_handle->GPIO_TX_Pin, GPIO_PIN_RESET);
 		uart_handle->TX_Bit_Count++;
@@ -83,36 +100,105 @@ void _104us_ISR()
 		uart_handle->TX_Bit_Count = 0;
 		uart_handle->TX_Active_Flag = 0;
 		}
+	    }
+
+
+/*
+
+	if (!uart_handle->RX_Active_Flag)
+	    {
+	    if (HAL_GPIO_ReadPin(uart_handle->GPIO_RX_Port,
+		    uart_handle->GPIO_RX_Pin) == GPIO_PIN_RESET)
+		{
+		uart_handle->RX_Active_Flag = 1;
+		}
+	    }
+
+	if (uart_handle->RX_Active_Flag)
+	    {
+
+	    uart_active_count++;
+
+	    if (!uart_handle->RX_Bit_Count)
+		{
+		uart_handle->RX_Bit_Count = 1; // start bit
+		}
+	    else if (uart_handle->RX_Bit_Count < 9)
+		{
+		uart_handle->RX_Byte |= HAL_GPIO_ReadPin(
+			uart_handle->GPIO_RX_Port, uart_handle->GPIO_RX_Pin)
+			<< (uart_handle->RX_Bit_Count - 1);
+		uart_handle->RX_Bit_Count++;
+		}
+	    else //stop bit
+		{
+		uart_handle->RX_Bit_Count = 0;
+		uart_handle->RX_Active_Flag = 0;
+		Ring_Buffer_Put_Char(rx_buffer_handle, uart_handle->RX_Byte);
+		uart_handle->RX_Byte = 0x00;
+		}
 
 	    }
+
+*/
 
 	}
 
     // if all soft_uart are idle disable tim interrupt
     if (!uart_active_count)
 	{
-	HAL_TIM_Base_Stop_IT(&SOFT_UART_TIM);
+	//HAL_TIM_Base_Stop_IT(&SOFT_UART_TIM);
 	}
 
     }
 
 void Soft_Uart_Send_Char(Soft_Uart_t* uart_handle, char data)
     {
-    Uart_Ring_Buffer* ring_buffer_handle = &uart_handle->TX_Ring_Buffer;
-    Ring_Buffer_Put_Char(ring_buffer_handle, data);
-    HAL_TIM_Base_Start_IT(&SOFT_UART_TIM);
+    Uart_Ring_Buffer* tx_buffer_handle = &uart_handle->TX_Ring_Buffer;
+    Ring_Buffer_Put_Char(tx_buffer_handle, data);
+    //HAL_TIM_Base_Start_IT(&SOFT_UART_TIM);
     }
 
 void Soft_Uart_Send_String(Soft_Uart_t* uart_handle, char* str)
     {
 
-    Uart_Ring_Buffer* ring_buffer_handle = &uart_handle->TX_Ring_Buffer;
+    Uart_Ring_Buffer* tx_buffer_handle = &uart_handle->TX_Ring_Buffer;
 
     while (*str != '\0')
 	{
-	Ring_Buffer_Put_Char(ring_buffer_handle, *str++);
+	Ring_Buffer_Put_Char(tx_buffer_handle, *str++);
 	}
+    //HAL_TIM_Base_Start_IT(&SOFT_UART_TIM);
+    }
+
+uint8_t Soft_Uart_Get_TC_Flag(Soft_Uart_t* uart_handle)
+    {
+    return uart_handle->TC_Flag;
+    }
+
+void Soft_Uart_Clear_TC_Flag(Soft_Uart_t* uart_handle)
+    {
+    uart_handle->TC_Flag = 0;
+    }
+
+uint16_t Soft_Uart_RX_Char_Count(Soft_Uart_t* uart_handle)
+    {
+    return Ring_Buffer_Get_Count(&uart_handle->RX_Ring_Buffer);
+    }
+
+uint8_t Soft_Uart_Read_RX_Char(Soft_Uart_t* uart_handle, char* data)
+    {
+    return Ring_Buffer_Get_Char(&uart_handle->RX_Ring_Buffer, data);
+    }
+
+void Soft_Uart_Start()
+    {
     HAL_TIM_Base_Start_IT(&SOFT_UART_TIM);
+    }
+
+void Soft_Uart_Stop()
+    {
+    HAL_TIM_Base_Stop_IT(&SOFT_UART_TIM);
     }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
